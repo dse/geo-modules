60bfde9a6598fa7abf0a1346a1cb5aeb1421dbf1
Darren Embry <dse@webonastick.com>
2014-06-29 21:30:23 -0400

    - fixes - get list of "versions" (mtimes) of the data - get list of dates - get list of versions by date - list trip updates

--- Geo-GTFS/lib/Geo/GTFS/Realtime.pm
+++ Geo-GTFS/lib/Geo/GTFS/Realtime.pm
@@ -8,14 +8,14 @@ use LWP::Simple;
 use HTTP::Cache::Transparent;
 use Google::ProtocolBuffers;
 use File::Path qw(make_path);
-use POSIX qw(strftime);
+use POSIX qw(strftime floor uname);
 use File::Basename qw(dirname);
 use JSON qw(-convert_blessed_universally);
 use YAML::Syck;
 use Data::Dumper;
 use Errno qw(EEXIST);
 use File::Spec::Functions qw(abs2rel);
-use POSIX qw(uname);
+use Time::Local qw(timegm);
 
 BEGIN {
     # in osx you may have to run: cpan Crypt::SSLeay and do other
@@ -161,7 +161,7 @@ sub pull {
     $latest{pb}     = { $make_hash_by_feed_type->(sub { sprintf("%s/%s/pb/latest.pb", $self->{my_cache}, $_         ) }) };
 
     foreach my $feed_type (@feed_types) {
-	_file_put_contents($filename{pb}{$feed_type}, $pb{$feed_type});
+	_file_put_contents($filename{pb}{$feed_type}, $pb{$feed_type}, "b");
     }
 
     # convert to other formats and write them
@@ -209,6 +209,135 @@ sub pull {
     # }
 }
 
+sub get_all_versions {
+    my ($self) = @_;
+    return if $self->{versions};
+
+    my $v = $self->{versions} = {};
+
+    my $dir = $self->{my_cache};
+    my @pb = glob("$dir/*/pb/????/??/??/??????Z.pb");
+    my %time_t;
+    my %date;
+    foreach my $pb (@pb) {
+	my $rel = abs2rel($pb, $dir);
+	if ($rel =~ m{(?:^|/)([^/]+)/pb/(\d+)/(\d+)/(\d+)/(\d\d)(\d\d)(\d\d)Z\.}) {
+	    my ($feed_type, $yyyy, $mm, $dd, $hh, $mi, $ss) = ($1, $2, $3, $4, $5, $6, $7);
+	    foreach ($yyyy, $mm, $dd, $hh, $mi, $ss) {
+		$_ =~ s{^0+}{};	# avoid octal parsing
+		$_ = $_ + 0;	# force to number
+	    }
+	    my $time_t = timegm($ss, $mi, $hh, $dd, $mm - 1, $yyyy);
+	    my $date = strftime("%Y-%m-%d", localtime($time_t));
+	    my $feed_type_rec = {
+		time_t => $time_t,
+		date => $date,
+		pb => $pb,
+	    };
+	    my $rec = {
+		time_t => $time_t,
+		date => $date,
+	    };
+	    $v->{$feed_type}->{date}->{$date}->{$time_t} = $feed_type_rec;
+	    $v->{$feed_type}->{time_t}->{$time_t} = $feed_type_rec;
+	    $v->{date}->{$date}->{$time_t} = $rec;
+	    $v->{time_t}->{$time_t} = $rec;
+	}
+    }
+}
+
+sub list_dates {
+    my ($self) = @_;
+    $self->get_all_versions();
+
+    my @date = reverse sort keys %{$self->{versions}->{date}};
+    if (scalar(@date) > 20) {
+	splice(@date, 20);
+    }
+    return @date;
+}
+
+sub list_versions_by_date {
+    my ($self, $date) = @_;
+    $self->get_all_versions();
+
+    my @time_t = reverse sort { $a <=> $b } keys %{$self->{versions}->{date}->{$date}};
+    if (scalar(@time_t) > 20) {
+	splice(@time_t, 20);
+    }
+    printf("  %12d => %s\n", $_, scalar(localtime($_))) foreach @time_t;
+    return @time_t;
+}
+
+sub list_trip_updates {
+    my ($self, $version) = @_;
+    $self->get_all_versions();
+
+    my $vp_pb_filename = $self->{versions}->{vehicle_positions}->{time_t}->{$version}->{pb};
+    my $vp_pb_encoded = _file_get_contents($vp_pb_filename, "b");
+    my $vp_pb = TransitRealtime::FeedMessage->decode($vp_pb_encoded);
+    my $vp_header_timestamp = $vp_pb->{header}->{timestamp};
+
+    my $tu_pb_filename = $self->{versions}->{trip_updates}->{time_t}->{$version}->{pb};
+    my $tu_pb_encoded = _file_get_contents($tu_pb_filename, "b");
+    my $tu_pb = TransitRealtime::FeedMessage->decode($tu_pb_encoded);
+    my $tu_header_timestamp = $tu_pb->{header}->{timestamp};
+
+    my $vp_header_time = strftime("%m/%d %H:%M:%S", localtime($vp_header_timestamp));
+    my $tu_header_time = strftime("%m/%d %H:%M:%S", localtime($tu_header_timestamp));
+
+    print("Trip updates      as of $tu_header_time\n");
+    print("Vehicle positions as of $tu_header_time\n");
+
+    my %vehicle;
+
+    foreach my $entity (@{$vp_pb->{entity}}) {
+	my $label     = eval { $entity->{vehicle}->{vehicle}->{label} } //
+	  eval { $entity->{id} };
+	my $timestamp = eval { $entity->{vehicle}->{timestamp} };
+	my $trip_id   = eval { $entity->{vehicle}->{trip}->{trip_id} };
+	next if !defined $label;
+	my $rec = {
+	    label => $label,
+	    ((defined $timestamp) ? (timestamp => $timestamp) : ()),
+	    ((defined $trip_id)   ? (trip_id   => $trip_id  ) : ()),
+	};
+	$vehicle{$label} = $rec;
+    }
+
+    foreach my $entity (@{$tu_pb->{entity}}) {
+	my $tu       = eval { $entity->{trip_update} };
+	next if !defined $tu;
+
+	my $trip_id  = eval { $tu->{trip}->{trip_id} } // eval { $tu->{id} };
+	my $route_id = eval { $tu->{trip}->{route_id} };
+	my $label    = eval { $tu->{vehicle}->{label} };
+	next if !defined $label;
+
+	my $start_time = eval { $tu->{trip}->{start_time} };
+	my $start_date = eval { $tu->{trip}->{start_date} };
+	my $delay = eval { $tu->{stop_time_update}->[0]->{departure}->{delay} };
+	$delay /= 60 if defined $delay;
+	my $dep_arr_time = eval { $tu->{stop_time_update}->[0]->{departure}->{time} } // eval { $tu->{stop_time_update}->[0]->{arrival}->{time} };
+	my $dep_arr_time_fmt = defined $dep_arr_time && eval { strftime("%m/%d %H:%M:%S", localtime($dep_arr_time)) };
+
+	my $vehicle = $vehicle{$label};
+	my $timestamp = eval { $vehicle->{timestamp} };
+	my $timestamp_fmt = defined $timestamp && eval { strftime("%m/%d %H:%M:%S", localtime($timestamp)) };
+	
+	next if !defined $timestamp;
+	next if $tu_header_timestamp - $timestamp >= 1800;
+
+	printf("    veh %-8s  trip_id %-8s  route %-8s  deptime %-14s  timestamp %-14s  delay %d\n",
+	       $label // "-",
+	       $trip_id // "-", 
+	       $route_id // "-",
+	       $dep_arr_time_fmt // "-",
+	       $timestamp_fmt // "-",
+	       floor(($delay // 0) + 0.5));
+    }
+}
+
 sub _symlink {
     my ($source, $dest) = @_;
     if (!-e $source) {
@@ -250,6 +379,25 @@ sub _file_put_contents {
     }
     print $fh $data;
     print STDERR ("Done.\n");
+}
+
+sub _file_get_contents {
+    my ($filename, $mode) = @_;
+    open(my $fh, "<", $filename) or do {
+	warn("  cannot read $filename: $!\n");
+	return;
+    };
+    if (defined $mode && $mode eq "b") {
+	binmode($fh);
+    }
+    my $data = "";
+    my $status;
+    while (($status = sysread($fh, $data, 4096, length($data)))) {
+    }
+    if (!defined $status) {
+	warn("error reading $filename: $!\n");
+    }
+    return $data;
 }
 
 sub _join_and {
