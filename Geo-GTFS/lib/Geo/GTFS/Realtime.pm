a92e0b9dcf3fb61c059d15cfca58e2fc37b4906a
Darren Embry <dse@webonastick.com>
2014-09-28 23:57:51 -0400

    cleanups; JSON::Encoder::Compact is its own module now.

--- Geo-GTFS/lib/Geo/GTFS/Realtime.pm
+++ Geo-GTFS/lib/Geo/GTFS/Realtime.pm
@@ -2,15 +2,14 @@ package Geo::GTFS::Realtime;	# -*- cperl -*-
 use warnings;
 use strict;
 
+use List::MoreUtils;
+
+use vars qw($MYHOME);
 BEGIN {
-    if (defined $ENV{REQUEST_METHOD}) {
-	unshift(@INC, "/home/dse/git/HTTP-Cache-Transparent/lib");
-	# my fork adds a special feature called NoUpdateImpatient.
-    } else {
-	unshift(@INC, "$ENV{HOME}/git/HTTP-Cache-Transparent/lib");
-	# my fork adds a special feature called NoUpdateImpatient.
-    }
+    $MYHOME = first_value { -d $_ } qw(/home/dse /Users/dse /tmp);
 }
+use lib "$MYHOME/git/HTTP-Cache-Transparent/lib";
+use lib "$MYHOME/git/JSON-Encoder-Compact/lib";
 
 # use Carp::Always;
 use Geo::GTFS;
@@ -26,6 +25,7 @@ use Data::Dumper;
 use Errno qw(EEXIST);
 use File::Spec::Functions qw(abs2rel);
 use Time::Local qw(timegm);
+use JSON::Encoder::Compact;
 use open IO => ":locale";
 use charnames ":full";
 
@@ -925,122 +925,5 @@ sub _dist_A_B {
 }
 
 ###############################################################################
-
-package My::JSON::Encoder::Compact;
-use JSON qw(-convert_blessed_universally);
-sub new {
-    my ($class, %args) = @_;
-    my $self = bless({ json => JSON->new()->allow_nonref()->pretty()->convert_blessed(),
-		       compactness => 1,
-		       %args }, $class);
-    return $self;
-}
-sub encode {
-    my ($self, $data) = @_;
-    if (ref($data)) {
-	$data = $self->{json}->encode($data);
-	$data = $self->{json}->decode($data);
-    }
-    my $encoded = "";
-    $self->__encode(stringref => \$encoded, data => $data);
-    return $encoded . "\n";
-}
-sub __encode {
-    my ($self, %args) = @_;
-
-    my $stringref     = $args{stringref};
-    my $data          = $args{data};
-    my $indent        = $args{indent}        // 0;
-    my $extra_compact = $args{extra_compact} // 0;
-    my $stack         = $args{stack}         // [];
-
-    my $ref = ref($data);
-    if (!$ref) {
-	my $enc = $self->{json}->encode($data);
-	chomp($enc);
-	$$stringref .= $enc;
-    } elsif ($ref eq "ARRAY") {
-	if (scalar(@$data) == 0) {
-	    $$stringref .= "[]";
-	} else {
-	    my $newlines = 1;
-	    if ($extra_compact) {
-		$newlines = 0;
-	    } elsif ($self->{compactness} >= 2) {
-		if (!grep { ref($_) } @$data) {
-		    $newlines = 0;
-		}
-	    }
-	    $$stringref .= "[ ";
-	    $indent += 2;
-
-	    my $extra_compact = $extra_compact || eval { $self->{extra_compact}->{$stack->[-1] . "[]"} };
-
-	    for (my $i = 0; $i <= scalar(@$data); $i += 1) {
-		if ($i) {
-		    if ($newlines) {
-			$$stringref .= ",\n" . (" " x $indent);
-		    } else {
-			$$stringref .= ", ";
-		    }
-		}
-		$self->__encode(stringref => $stringref,
-				data => $data->[$i],
-				indent => $indent,
-				extra_compact => $extra_compact,
-				stack => [@$stack, "[]"]);
-	    }
-	    $$stringref .= " ]";
-	}
-    } elsif ($ref eq "HASH") {
-	my @keys = keys(%$data);
-	if (scalar(@keys) == 0) {
-	    $$stringref .= "{}";
-	} else {
-	    my $newlines = 1;
-	    if ($extra_compact) {
-		$newlines = 0;
-	    } elsif ($self->{compactness} >= 2) {
-		if (!grep { ref($_) } values(%$data)) {
-		    $newlines = 0;
-		}
-	    }
-	    $$stringref .= "{ ";
-	    $indent += 2;
-	    for (my $i = 0; $i < scalar(@keys); $i += 1) {
-		my $key = $keys[$i];
-		my $extra_compact = $extra_compact || eval { $self->{extra_compact}->{$key} };
-		my $value = $data->{$key};
-		my $sub = eval { $self->{convert_key}->{$key} };
-		if ($sub && ref($sub) eq "CODE") {
-		    my $new_value = $sub->($value);
-		    next if !defined $new_value;
-		    $value = $new_value;
-		}
-		if ($i) {
-		    if ($newlines) {
-			$$stringref .= ",\n" . (" " x $indent);
-		    } else {
-			$$stringref .= ", ";
-		    }
-		}
-		my $key_enc = $self->{json}->encode($key);
-		chomp($key_enc);
-		$$stringref .= $key_enc;
-		$$stringref .= ": ";
-		$self->__encode(stringref => $stringref,
-				data => $value,
-				indent => $indent + length($key_enc) + 2,
-				extra_compact => $extra_compact,
-				stack => [@$stack, $key]);
-	    }
-	    $$stringref .= " }";
-	}
-    } else {
-	my $enc = $self->{json}->encode("$data");
-	chomp($enc);
-	$$stringref .= $enc;
-    }
-}
 
 1;
