35a04faa463ab017150a643bf5abd3aea4524077
Darren Embry <dse@webonastick.com>
2019-08-31 01:45:17 -0400

    add log level constants

--- Geo-MapMaker/lib/Geo/MapMaker/Constants.pm
+++ Geo-MapMaker/lib/Geo/MapMaker/Constants.pm
@@ -10,6 +10,11 @@ use constant POINT_Y => 1;
 use constant POINT_X_ZONE => 2;
 use constant POINT_Y_ZONE => 3;
 
+use constant LOG_ERROR => 0;
+use constant LOG_WARN  => 1;
+use constant LOG_INFO  => 2;
+use constant LOG_DEBUG => 3;
+
 use base qw(Exporter);
 use Exporter;
 
@@ -18,7 +23,11 @@ our @EXPORT_OK = qw(FALSE
                     POINT_X
                     POINT_Y
                     POINT_X_ZONE
-                    POINT_Y_ZONE);
+                    POINT_Y_ZONE
+                    LOG_ERROR
+                    LOG_WARN
+                    LOG_INFO
+                    LOG_DEBUG);
 
 our %EXPORT_TAGS = (
     all => [qw(FALSE
@@ -26,7 +35,11 @@ our %EXPORT_TAGS = (
                POINT_X
                POINT_Y
                POINT_X_ZONE
-               POINT_Y_ZONE)]
+               POINT_Y_ZONE
+               LOG_ERROR
+               LOG_WARN
+               LOG_INFO
+               LOG_DEBUG)]
 );
 
 1;
