48efcd19285717140b1580f2f503ef5f4db6cdc8
Darren Embry <dse@webonastick.com>
2019-09-05 13:30:52 -0400

    svg_object and is_closed methods

--- Geo-MapMaker/lib/Geo/MapMaker/OSM/Way.pm
+++ Geo-MapMaker/lib/Geo/MapMaker/OSM/Way.pm
@@ -6,12 +6,37 @@ use v5.10.0;
 use lib "$ENV{HOME}/git/dse.d/geo-modules/Geo-MapMaker/lib";
 use base 'Geo::MapMaker::OSM::Object';
 
+use Geo::MapMaker::Constants qw(:all);
+use Geo::MapMaker::SVG::PolyLine;
+use List::MoreUtils qw(all);
+use Data::Dumper qw(Dumper);
+
 sub new {
     my $class = shift;
     my $self = $class->SUPER::new(@_);
     bless($self, $class);
 
     return $self;
+}
+
+sub svg_object {
+    my ($self, %args) = @_;
+    my $map_area_index = $args{map_area_index} || 0;
+    my $is_closed = $args{is_closed} // $self->{is_closed};# // $self->is_closed;
+    my @svg_coords = grep { $_ } map { $_->{svg_coords}->[$map_area_index] } @{$self->{node_array}};
+    return unless scalar @svg_coords;
+    if (all { $_->[POINT_X_ZONE] == -1 } @svg_coords) { return; }
+    if (all { $_->[POINT_X_ZONE] ==  1 } @svg_coords) { return; }
+    if (all { $_->[POINT_Y_ZONE] == -1 } @svg_coords) { return; }
+    if (all { $_->[POINT_Y_ZONE] ==  1 } @svg_coords) { return; }
+    my $polyline = Geo::MapMaker::SVG::PolyLine->new(@svg_coords);
+    return $polyline;
+}
+
+sub is_closed {
+    my ($self) = @_;
+    return scalar@{$self->{node_array}} > 1 &&
+        $self->{node_array}->[0]->{-id} eq $self->{node_array}->[-1]->{-id};
 }
 
 1;
