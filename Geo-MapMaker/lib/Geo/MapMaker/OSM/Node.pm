31edd39ce317c6d894e2b359632c33ddd0512c76
Darren Embry <dse@webonastick.com>
2019-09-09 17:43:24 -0400

    cleanup

--- Geo-MapMaker/lib/Geo/MapMaker/OSM/Node.pm
+++ Geo-MapMaker/lib/Geo/MapMaker/OSM/Node.pm
@@ -6,14 +6,6 @@ use v5.10.0;
 use lib "$ENV{HOME}/git/dse.d/geo-modules/Geo-MapMaker/lib";
 use base 'Geo::MapMaker::OSM::Object';
 
-sub new {
-    my $class = shift;
-    my $self = $class->SUPER::new(@_);
-    bless($self, $class);
-
-    return $self;
-}
-
 sub svg_object {
     # placeholder
     return;
