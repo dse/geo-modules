4faa6f667ffd58bd65e9b131e83d7d424a6b331e
Darren Embry <dse@webonastick.com>
2019-09-09 15:33:06 -0400

    add normalize_space

--- Geo-MapMaker/lib/Geo/MapMaker/Util.pm
+++ Geo-MapMaker/lib/Geo/MapMaker/Util.pm
@@ -12,7 +12,9 @@ use base 'Exporter';
 use vars qw(@EXPORT @EXPORT_OK);
 
 @EXPORT = qw();
-@EXPORT_OK = qw(file_get_contents file_put_contents);
+@EXPORT_OK = qw(file_get_contents file_put_contents normalize_space);
+
+use Text::Trim;
 
 sub file_get_contents {		# php-like lol
     my ($filename) = @_;
@@ -24,6 +26,13 @@ sub file_put_contents {		# php-like lol
     my ($filename, $contents) = @_;
     open(my $fh, ">", $filename) or die("Cannot write $filename: $!\n");
     print $fh $contents;
+}
+
+sub normalize_space {
+    my ($string) = @_;
+    $string = trim($string);
+    $string =~ s{\s+}{ }g;
+    return $string;
 }
 
 1;
